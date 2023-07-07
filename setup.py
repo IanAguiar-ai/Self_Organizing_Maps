51e66a5186334b4fd7a032b59053de76397ef68a
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-07-06 22:41:49 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Self Organizing Maps'
 LONG_DESCRIPTION = 'The library was built to run SOM'
 
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     install_requires=['matplotlib', 'numpy', 'pandas'],
-    keywords=['python', 'som', 'clustering'],
+    keywords=['python', 'som', 'SOM', 'clustering'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
